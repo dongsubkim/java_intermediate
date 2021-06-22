ae80440e0bec43f3ea488d54825455679222a6c8
DongSubKim <dskim9218@gmail.com>
2021-06-22 22:56:16 +0900

    part 3 generic

---
+++ Part_3/Box.java
@@ -0,0 +1,14 @@
+package Part_3;
+
+public class Box<E> {
+    private E obj;
+
+    public void setObj(E obj) {
+        this.obj = obj;
+    }
+
+    public E getObj() {
+        return this.obj;
+    }
+
+}
