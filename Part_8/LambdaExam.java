bda2406e5cb705b0c018d5a23404a1f32241eaa1
DongSubKim <dskim9218@gmail.com>
2021-06-23 03:04:52 +0900

    part 8 lambda expression

---
+++ Part_8/LambdaExam.java
@@ -0,0 +1,14 @@
+package Part_8;
+
+public class LambdaExam {
+
+    public static void main(String[] args) {
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("hello");
+            }
+        }).start();
+    }
+
+}
