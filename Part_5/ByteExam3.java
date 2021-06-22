6be663eb8afebf5ce82213d7fcb6d6910a9cd5a8
DongSubKim <dskim9218@gmail.com>
2021-06-23 00:33:44 +0900

    part 5 byte 3

---
+++ Part_5/ByteExam3.java
@@ -0,0 +1,16 @@
+package Part_5;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+
+public class ByteExam3 {
+    public static void main(String[] args) {
+        try (DataOutputStream out = new DataOutputStream(new FileOutputStream("data.txt"));) {
+            out.writeInt(100);
+            out.writeBoolean(true);
+            out.writeDouble(50.5);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
